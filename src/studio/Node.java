ad6990adf8d8689476aebaec241cb38c637dd99d
Samiel <163655207+SamielXD@users.noreply.github.com>
2026-01-03 19:50:22 +0800

    Update Node.java

--- src/studio/Node.java
+++ src/studio/Node.java
@@ -5,80 +5,87 @@ import arc.math.geom.*;
 import arc.struct.*;
 
 public class Node {
+    public String id;
     public String type;
     public String label;
     public float x, y;
-    public float width = 150f, height = 80f;
-    public Color color;
-    public Seq<Node> connections = new Seq<>();
+    public float width = 400f;
+    public float height = 200f;
     public String value = "";
-    public String id;
+    public Color color = Color.gray;
+    
+    public Seq<Node> connections = new Seq<>();
     public Seq<NodeInput> inputs = new Seq<>();
-    
-    public Node() {}
-    
+
+    public Node() {
+        this.id = java.util.UUID.randomUUID().toString();
+    }
+
     public Node(String type, String label, float x, float y, Color color) {
+        this();
         this.type = type;
         this.label = label;
         this.x = x;
         this.y = y;
         this.color = color;
-        this.id = java.util.UUID.randomUUID().toString();
-        
         setupInputs();
     }
-    
+
     public void setupInputs() {
         inputs.clear();
         
         if(label.equals("Message")) {
-            inputs.add(new NodeInput("text", "Message", "Hello!"));
+            inputs.add(new NodeInput("Text", "Hello!"));
         }
         else if(label.equals("Spawn Unit")) {
-            inputs.add(new NodeInput("text", "Unit Type", "dagger"));
+            inputs.add(new NodeInput("Unit Type", "dagger"));
+            inputs.add(new NodeInput("Spawn Location", "core")); // NEW: spawn location
+            inputs.add(new NodeInput("X Coordinate", "0")); // NEW: X position
+            inputs.add(new NodeInput("Y Coordinate", "0")); // NEW: Y position
+            inputs.add(new NodeInput("Amount", "1")); // NEW: how many to spawn
         }
         else if(label.equals("Set Block")) {
-            inputs.add(new NodeInput("text", "X,Y,Block", "10,10,router"));
+            inputs.add(new NodeInput("Position (x,y,blockname)", "10,10,copper-wall"));
         }
         else if(label.equals("Wait")) {
-            inputs.add(new NodeInput("number", "Seconds", "1"));
+            inputs.add(new NodeInput("Seconds", "1"));
+        }
+        else if(label.equals("If")) {
+            inputs.add(new NodeInput("Condition", "true"));
         }
         else if(label.equals("Number")) {
-            inputs.add(new NodeInput("number", "Value", "0"));
+            inputs.add(new NodeInput("Value", "0"));
         }
         else if(label.equals("Text")) {
-            inputs.add(new NodeInput("text", "Value", ""));
+            inputs.add(new NodeInput("Value", ""));
         }
         else if(label.equals("Unit Type")) {
-            inputs.add(new NodeInput("text", "Type", "dagger"));
+            inputs.add(new NodeInput("Unit Name", "dagger"));
+        }
+        else if(label.equals("On Start")) {
+            value = "On Start";
+        }
+        else if(label.equals("On Wave")) {
+            value = "On Wave";
+        }
+        else if(label.equals("On Unit Spawn")) {
+            value = "On Unit Spawn";
         }
     }
-    
-    public boolean contains(float px, float py) {
-        return px >= x && px <= x + width && py >= y && py <= y + height;
-    }
-    
-    public Vec2 getCenter() {
-        return new Vec2(x + width/2f, y + height/2f);
-    }
-    
+
     public Vec2 getInputPoint() {
-        return new Vec2(x, y + height/2f);
+        return new Vec2(x, y + height / 2);
     }
-    
+
     public Vec2 getOutputPoint() {
-        return new Vec2(x + width, y + height/2f);
+        return new Vec2(x + width, y + height / 2);
     }
-    
+
     public static class NodeInput {
-        public String type;
         public String label;
         public String value;
-        
-        public NodeInput() {}
-        
-        public NodeInput(String type, String label, String defaultValue) {
-            this.type = type;
+
+        public NodeInput(String label, String defaultValue) {
             this.label = label;
             this.value = defaultValue;
         }
